56ecf1f3c9f5b99419e2c59685ff36bd8190dd59
alikhanov <alikhanov8608@gmail.com>
2026-02-08 23:53:19 +0500

    fix cors for front

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
-require('dotenv').config()
-const app = require('./src/app')
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
-})
+const app = express()
+
+app.use(cors({
+  origin: "https://electronics-store-frontend.onrender.com",
+  credentials: true
+}))
+
+app.use(express.json())
+
+// routes
+app.use("/api/auth", require("./src/routes/authRoutes"))
+app.use("/api/products", require("./src/routes/productRoutes"))
+app.use("/api/users", require("./src/routes/userRoutes"))
+
+const connectDB = require("./src/config/db")
+connectDB()
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
